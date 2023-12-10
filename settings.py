6b54647e9a5f058dcaf62a39b867da11e887f706
zinc-96 <96zn1231@gmail.com>
2023-12-10 17:35:46 +0800

    修复bug，功能基本完成

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 DATABASE = 'HIS'  # 选择数据库名称
-USER = 'test' # 数据库用户名
-PASSWORD = 'testGauss.' # 数据库密码
+USER = 'test'  # 数据库用户名
+PASSWORD = 'testGauss.'  # 数据库密码
 HOST = '121.36.55.115'  # 数据库ip
-PORT = '5432' # 数据库端口
+PORT = '5432'  # 数据库端口
 photo_path = r"static/images/"  # 照片存放路径
-hospital_info = { # 模拟医院基本信息数据
+hospital_info = {  # 模拟医院基本信息数据
     'name': 'Example Hospital',
     'address': '123 Main St',
     'phone': '555-1234',
@@ -33,6 +33,14 @@ db_log = {
         'port': '5432'
 
     },
+    'nurse': {
+        'database': 'HIS',
+        'user': 'test',
+        'password': 'testGauss.',
+        'host': '121.36.55.115',
+        'port': '5432'
+
+    },
     'drugadmin': {
         'database': 'HIS',
         'user': 'test',
@@ -41,7 +49,7 @@ db_log = {
         'port': '5432'
 
     },
-    'pharmacy_nurse': {
+    'supplier': {
         'database': 'HIS',
         'user': 'test',
         'password': 'testGauss.',
